2211e9cc2fb8ab14e8a2806cf4a5dbd106c836ab
awuradjoaq <awuradjoaq@gmail.com>
2021-01-09 15:13:03 -0800

    fixed type errors in PostDisplay.jsx

--- client/src/components/PostDisplay.tsx
+++ client/src/components/PostDisplay.tsx
@@ -1,29 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import BlogPost from './BlogPost';
 
 interface BlogPostProps {
-  post: {
     id: number;
     title: string;
     username_id: number;
     username: string;
     date_created: string;
     post: string;
-  };
-}
+};
 
 const PostDisplay: React.FC = (props: any) => {
 
-  const [display, setDisplay] = useState([]);
-
-  console.log(display);
+  const [display, setDisplay] = useState<BlogPostProps | null>(null);
 
   useEffect(() => {
     axios.get(`/blog/${props.match.params.id}`)
     .then(result => setDisplay(result.data[0]))
     .catch(error => console.log(error));
   },[]);
-  return <span>{display.post}</span>;
+  if (display) {
+    return <span>{display.post}</span>;
+  } else {
+    return null;
+  }
 
 };
 

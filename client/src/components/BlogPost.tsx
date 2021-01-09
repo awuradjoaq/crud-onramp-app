101ea47028e6f2d561c9bfd011231eae982d7c60
awuradjoaq <awuradjoaq@gmail.com>
2021-01-09 00:59:12 -0800

    refactored Search.jsx, BlogPost.jsx to functional components

--- client/src/components/BlogPost.tsx
+++ client/src/components/BlogPost.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 interface BlogPostProps {
   post: {
@@ -9,32 +9,23 @@ interface BlogPostProps {
     title: string;
     post: string;
     date_posted: string;
-  }
+  };
 }
 
 interface BlogViewState {
   postViewed: boolean;
 }
 
-class BlogPost extends React.Component<BlogPostProps, BlogViewState> {
-  constructor(props: BlogPostProps) {
-    super(props);
-    this.state = {
-      postViewed: false
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <Link to='blogpost/:id'>
-          <h2>{this.props.post.title}</h2>
-        </Link>
-        <h3>{this.props.post.username}</h3>
-        <h4>{this.props.post.date_posted}</h4>
-      </div>
-    );
-  }
+const BlogPost: React.FC<BlogPostProps> = (props) => {
+  return (
+    <div>
+      <Link to="blogpost/:id">
+        <h2>{props.post.title}</h2>
+      </Link>
+      <h3>{props.post.username}</h3>
+      <h4>{props.post.date_posted}</h4>
+    </div>
+  );
 };
 
 export default BlogPost;
--- client/src/components/PostDisplay.tsx
+++ client/src/components/PostDisplay.tsx
@@ -1,15 +1,5 @@
 import React from 'react';
 
-// interface BlogPostProps {
-//   post: {
-//     username_id: number;
-//     username: string;
-//     blog_post_id: number;
-//     title: string;
-//     post: string;
-//     date_posted: string;
-//   }
-// }
 
 const PostDisplay: React.FC = (props) => (
   <h1>I work!</h1>
